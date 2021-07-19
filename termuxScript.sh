ebde290433402e27b8bec908f78f30765ddd63b0
Kavin Sood <68739182+k2s09@users.noreply.github.com>
2021-07-19 21:00:40 +0530

    Update termuxScript.sh

--- termuxScript.sh
+++ termuxScript.sh
@@ -7,9 +7,9 @@ cd /data/data/com.termux/files/usr/bin
 cat script >> java
 chmod +x java
 cd ~
-echo Done!
-echo Now open $choice and code! When you are done, exit the editor and do
-echo java -r <filename>
-echo If the file is called hello.java, do java -r hello. Do not include the file extension
-echo If your code does not have any errors, the file will compile and run.
-echo If you code has errors, they will be displayed. Fix them in your editor and try again.
+echo "Done!"
+echo "Now open $choice and code! When you are done, exit the editor and do"
+echo "java -r <filename>"
+echo "If the file is called hello.java, do java -r hello. Do not include the file extension"
+echo "If your code does not have any errors, the file will compile and run."
+echo "If you code has errors, they will be displayed. Fix them in your editor and try again."
